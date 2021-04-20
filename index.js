84893424f217f7096f2104cf9c04b2e03afc83b0
kecsou <kevin.voyer.developpeur@gmail.com>
2021-04-20 02:19:28 +0200

    feat: add check for port environnement variable

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 
 const app = express();
-const PORT = process.env.port || 80;
+const PORT = Number(process.env.port || 80);
+
+if (isNaN(PORT) && !isFinite(PORT)) {
+    console.error(`Value ${PORT} must be a number`);
+    process.exit(1);
+}
+if (PORT !== 80) {
+    if (PORT < 1024 || PORT > 49151) {
+        console.error(`Your port ${PORT} must be in the range [1024, 49151]`);
+        process.exit(1);
+    }
+}
 
 app.use(helmet());
 
